1a4ccc3caa286b9fee8a243efa5870987aefea66
Luiz Felippe Augusto <siipecapoeira@gmail.com>
2025-11-16 22:57:38 -0300

    feat: bombs disappear

--- src/scenes/game.scene3.ts
+++ src/scenes/game.scene3.ts
@@ -1,3 +1,10 @@
+enum PlayerState {
+  Vulnerable,
+  InvincibleBomb,
+  InvinciblePowerUp,
+  Dead,
+}
+
 export class GameScene extends Phaser.Scene {
   private bgMusic!: Phaser.Sound.BaseSound;
   private starSound!: Phaser.Sound.BaseSound;
@@ -5,6 +12,7 @@ export class GameScene extends Phaser.Scene {
   private gameoverSound!: Phaser.Sound.BaseSound;
   private invincibleSound!: Phaser.Sound.BaseSound;
   private powerUpSound!: Phaser.Sound.BaseSound;
+  private bombDisappearSound!: Phaser.Sound.BaseSound;
   private platforms!: Phaser.Physics.Arcade.StaticGroup;
   private player!: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
   private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
@@ -16,8 +24,7 @@ export class GameScene extends Phaser.Scene {
   private blinkEvent?: Phaser.Time.TimerEvent;
   private score = 0;
   private lives = 5;
-  private isInvincible = false;
-  private gameover = false;
+  private playerState = PlayerState.Vulnerable;
 
   constructor() {
     super('scene-game');
@@ -30,6 +37,7 @@ export class GameScene extends Phaser.Scene {
     this.load.audio('gameoverSound', 'assets2/gameover.mp3');
     this.load.audio('invincibleSound', 'assets2/invincible.mp3');
     this.load.audio('powerUpSound', 'assets2/powerUp.mp3');
+    this.load.audio('bombDisappearSound', 'assets2/buiu.mp3');
 
     this.load.image('sky', 'assets2/sky.png');
     this.load.image('ground', 'assets2/platform.png');
@@ -45,6 +53,7 @@ export class GameScene extends Phaser.Scene {
     this.gameoverSound = this.sound.add('gameoverSound');
     this.invincibleSound = this.sound.add('invincibleSound');
     this.powerUpSound = this.sound.add('powerUpSound');
+    this.bombDisappearSound = this.sound.add('bombDisappearSound');
 
     this.bgMusic.play({ loop: true });
 
@@ -123,18 +132,18 @@ export class GameScene extends Phaser.Scene {
   }
 
   update() {
-    if (this.gameover) {
+    if (this.playerState === PlayerState.Dead) {
       return;
     }
 
-    const { left, right, space, shift } = this.cursors;
+    const { left, right, space } = this.cursors;
 
     if (left.isDown) {
-      this.player.setVelocityX(shift.isDown ? -300 : -160);
+      this.player.setVelocityX(-300);
 
       this.player.anims.play('left', true);
     } else if (right.isDown) {
-      this.player.setVelocityX(shift.isDown ? 300 : 160);
+      this.player.setVelocityX(400);
 
       this.player.anims.play('right', true);
     } else {
@@ -172,11 +181,13 @@ export class GameScene extends Phaser.Scene {
           .setVelocity(70, 70)
           .setCollideWorldBounds(true);
 
-        childImage.body!.checkCollision.none = true;
+        if (this.stars.children.size <= 2) {
+          childImage.body!.checkCollision.none = true;
 
-        this.time.delayedCall(1_000, () => {
-          childImage.body!.checkCollision.none = false;
-        });
+          this.time.delayedCall(1_000, () => {
+            childImage.body!.checkCollision.none = false;
+          });
+        }
 
         starX += 150;
 
@@ -200,21 +211,34 @@ export class GameScene extends Phaser.Scene {
   }
 
   private hitPowerUp() {
-    if (this.isInvincible) {
+    if (this.playerState === PlayerState.InvinciblePowerUp) {
       return;
     }
 
     this.powerUp.disableBody(true, true);
     this.powerUpSound.play();
-    this.makeInvincible();
+    this.makeInvinciblePowerUp();
   }
 
-  private hitBomb() {
-    if (this.isInvincible) {
+  private hitBomb(
+    _: unknown,
+    bomb:
+      | Phaser.Types.Physics.Arcade.GameObjectWithBody
+      | Phaser.Physics.Arcade.Body
+      | Phaser.Physics.Arcade.StaticBody
+      | Phaser.Tilemaps.Tile
+  ) {
+    if (this.playerState === PlayerState.InvincibleBomb) {
       return;
     }
 
-    this.makeInvincible(1_000, false);
+    if (this.playerState === PlayerState.InvinciblePowerUp) {
+      (bomb as Phaser.Physics.Arcade.Image).destroy();
+      this.bombDisappearSound.play();
+      return;
+    }
+
+    this.makeInvincibleBomb();
     this.cameras.main.shake(100, 0.01);
     this.lives--;
     this.livesText.setText('❤️ '.repeat(this.lives));
@@ -226,7 +250,7 @@ export class GameScene extends Phaser.Scene {
       this.physics.pause();
       this.player.anims.play('turn');
       this.stopBlinking();
-      this.gameover = true;
+      this.playerState = PlayerState.Dead;
     } else {
       this.bombSound.play();
       this.time.delayedCall(500, () => this.player.clearTint());
@@ -252,25 +276,35 @@ export class GameScene extends Phaser.Scene {
     this.player.setAlpha(1);
   }
 
-  private makeInvincible(delay: number = 5000, changeSongs = true) {
-    if (this.isInvincible) {
+  private makeInvinciblePowerUp() {
+    if (this.playerState === PlayerState.InvinciblePowerUp) {
       return;
     }
 
-    this.isInvincible = true;
+    this.playerState = PlayerState.InvinciblePowerUp;
     this.startBlinking();
-    if (changeSongs) {
-      this.bgMusic.pause();
-      this.invincibleSound.play();
+    this.bgMusic.pause();
+    this.invincibleSound.play();
+
+    this.time.delayedCall(5_000, () => {
+      this.playerState = PlayerState.Vulnerable;
+      this.stopBlinking();
+      this.invincibleSound.stop();
+      this.bgMusic.play({ loop: true });
+    });
+  }
+
+  private makeInvincibleBomb() {
+    if (this.playerState === PlayerState.InvincibleBomb) {
+      return;
     }
 
-    this.time.delayedCall(delay, () => {
-      this.isInvincible = false;
+    this.playerState = PlayerState.InvincibleBomb;
+    this.startBlinking();
+
+    this.time.delayedCall(1_000, () => {
+      this.playerState = PlayerState.Vulnerable;
       this.stopBlinking();
-      if (changeSongs) {
-        this.invincibleSound.stop();
-        this.bgMusic.play({ loop: true });
-      }
     });
   }
 }
